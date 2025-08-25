47420b9d638b8848dcc37c98268172bc4509a231
DilshanHMT <156901370+DilshanHMT@users.noreply.github.com>
2025-08-25 20:17:43 +0500

    Update apiConstants.tsx

--- src/constants/apiConstants.tsx
+++ src/constants/apiConstants.tsx
@@ -1,2 +1,2 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://192.168.100.195:8000';
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://registration-backend-zvf6.onrender.com';
 export const BASE_URL_PREFIX = import.meta.env.VITE_API_PREFIX || 'api/v1';
